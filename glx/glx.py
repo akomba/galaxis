1d3780444fd39b3111aeeeee6854b504fb38c867
user@linux <user@debian>
2025-10-22 10:44:52 +0200

    rewamped glxloop, includes socketio as well

--- glx/glx.py
+++ glx/glx.py
@@ -10,13 +10,15 @@ from glx.collection import Collection
 import importlib
 import argparse
 
-__version__ = "0.4.3"
+__version__ = "0.4.4"
 
 def main():
 
     parser = argparse.ArgumentParser()
     parser.add_argument("-v", "--version", action="store_true")
     parser.add_argument("-c", "--community")
+    parser.add_argument("process", nargs='?')
+    parser.add_argument("-a", "--attribute")
     parser.add_argument("-l", "--list")
     parser.add_argument('init', nargs='?')
     args = parser.parse_args()
@@ -27,6 +29,17 @@ def main():
     if args.version:
         print(__version__)
         exit(0)
+    
+    if "communities" in sys.argv[1:]:
+        for community in communities:
+            print(community)
+        exit(0)
+
+    if args.community:
+        if not args.community in helper.communities():
+            print("unknown community:",args.community)
+            exit(0)
+     
     if args.init:
         community_name = input("community name (no spaces pls): ")
 
@@ -74,42 +87,24 @@ def main():
         }
         config = helper.load_or_create_app_config(community_name,"scheduler",config_template)
 
-
         #############################################
 
         c = Community(config["COMMUNITY_NAME"])
         print("name:",c.name )
         exit(0)
 
-    if "communities" in sys.argv[1:]:
-        for community in communities:
-            print(community)
-        exit(0)
+    if args.process and args.community:
+        community = args.community
+        lc = helper.load_local_config()
+        if not community in lc:
+            lc[community] = 0
+        iteration = lc[community]
 
-    elif args.community and args.list:
-        if args.list == "attributes":
-            collection = Collection(args.community,1)
-            helper.list_options(collection.attributes(raw=True))
-        exit(0)
-
-    # try to get apps
-    # don't worry how the apps get there
-    # read the config and execute based on that
-    # get a counter going
-    # and execute scripts when the modulo is zero
-    
-    # local config to read iterations from
-    lc = helper.load_local_config()
-    print("ITER:",lc["iteration"])
-    for community in communities:
-        config = helper.load_community_config(community)
-        apps = config["apps_folder"]
-        for app in os.listdir(apps):
+        for appname,conf in apps(community).items():
             # read config file
-            conf = helper.load_app_config(community,app)
             if "repeat" in conf:
-                if conf["repeat"] == 1 or lc["iteration"] % conf["repeat"] == 0:
-                    print("*",community,"> running",app,"(every",conf["repeat"],"iterations)")
+                if conf["repeat"] == 1 or iteration % conf["repeat"] == 0:
+                    print("*",community,"> running",appname,"(every",conf["repeat"],"iterations)")
                     # check for module info in the config file
                     if "module" in conf:
                         mname = conf["module"]
@@ -117,9 +112,48 @@ def main():
                         mname = app
                     m = importlib.import_module(mname+"."+app)
                     m.main(community)
-    # increase iteration
-    lc["iteration"] += 1
-    helper.save_local_config(lc)
+    
+        # increase iteration
+        lc[community] += 1
+        helper.save_local_config(lc)
+
+    elif args.community and args.list:
+        if args.list == "attributes":
+            collection = Collection(args.community,1)
+            helper.list_options(collection.attributes(raw=True))
+        elif args.list == "apps":
+            for k,v in apps(args.community).items():
+                print(k)
+            if not apps(args.community):
+                print("no apps found")
+        else:
+            print("unknown option:",args.list)
+        exit(0)
+
+    elif args.community and args.attribute:
+        collection = Collection(args.community,1)
+        attribute = collection.attribute(args.attribute)
+        print(attribute.name)
+        print("Cards:")
+        for instance in attribute.instances():
+            print("   "+str(instance["card_id"]))
+        exit(0)
+    # try to get apps
+    # don't worry how the apps get there
+    # read the config and execute based on that
+    # get a counter going
+    # and execute scripts when the modulo is zero
+    
+    # local config to read iterations from
+
+def apps(community):
+    config = helper.load_community_config(community)
+    apps = {}
+    for app in os.listdir(config["apps_folder"]):
+        conf = helper.load_app_config(community,app)
+        if conf:
+            apps[app] =  conf
+    return apps
 
 if __name__ == "__main__":
     main()
